1c4fd4d01b39f32e2ae28234d00202583c32c7d7
CHANATIP ATCHARIYABANCHA <chanatip_atcha@cmu.ac.th>
2019-03-11 16:31:09 +0700

    second commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log('This is my 1st JS');
 var str = '';
 for(var i = 0; i<10; i++){
     console.log(i+1);
-    str += (i+1) +'<br>';
+    str += (i+1) +'  ';
 }
 
 document.querySelector('#output').innerHTML = str;
